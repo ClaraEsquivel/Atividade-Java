119ccf25a3751077cfbc6b080c55a9f3430a056f
ClaraEsquivel <160122503+ClaraEsquivel@users.noreply.github.com>
2024-08-12 10:57:59 -0300

    Add files via upload

---
+++ processadormemoria_12_08/ProcessadorMemoria_12_08.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.processadormemoria_12_08;
+
+/**
+ *
+ * @author aluno.den
+ */
+public class ProcessadorMemoria_12_08 {
+
+    public static void main(String[] args) {
+        Processasor processador = new Processasor("AMD", "Ryzen 7", "3.8Ghz");
+        Memoria memoria = new Memoria("Kingston", "FURY", "12GB");
+       
+        
+        System.out.println("= = PROCESSADOR = =");
+        System.out.println("Marca: " + processador.getMarca());
+        System.out.println("Modelo: " + processador.getModelo());
+        System.out.println("Frequência: " + processador.getFrequencia());
+        
+        System.out.println("\n= = MEMÓRIA = =");
+        System.out.println("Marca: " + memoria.getMarca());
+        System.out.println("Modelo: " + memoria.getModelo());
+        System.out.println("Capacidade de Armazenamento: " + memoria.getCapacidadeArmazenamento());
+        
+    }
+}
---
+++ processadormemoria_12_08/Produto.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.processadormemoria_12_08;
+
+/**
+ *
+ * @author aluno.den
+ */
+public abstract class Produto {
+    //abstract - dizer que é classe modelo
+    protected String marca;
+    protected String modelo;
+    //ao inves de privado ficou protected
+
+    public Produto(String marca, String modelo) {
+        this.marca = marca;
+        this.modelo = modelo;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+    
+    
+}
